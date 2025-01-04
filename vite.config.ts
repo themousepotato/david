1be284e0aa4c816707e6bc5d0a1e78c93a6e3ff5
Navaneeth Suresh <navaneeths1998@gmail.com>
2025-01-05 00:11:10 +0530

    fix: add base david to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/david/',
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
